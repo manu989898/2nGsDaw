5489c4d5b03d1762189d7dab46fdfba993e5e2da
manu989898 <apkmanu1@gmail.com>
2024-10-14 17:13:54 +0200

    cosas

--- cliente/ejemplos/app.js
+++ cliente/ejemplos/app.js
@@ -95,3 +95,38 @@ export function RecipeList() {
   );
 }
 
+
+const poem = {
+    lines: [
+      'I write, erase, rewrite',
+      'Erase again, and then',
+      'A poppy blooms.'
+    ]
+  };
+  
+  export function Poem() {
+    let output = [];
+  
+    // Fill the output array
+    poem.lines.forEach((line, i) => {
+      output.push(
+        <hr key={i + '-separator'} />
+      );
+      output.push(
+        <p key={i + '-text'}>
+          {line}
+        </p>
+      );
+    });
+    // Remove the first <hr />
+    output.shift();
+  
+    return (
+      <article>
+        {output}
+      </article>
+    );
+  }
+  
+  
+
