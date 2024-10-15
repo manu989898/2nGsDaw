836b5d7c384b7fcb6cd22a405939934d09c49882
manu989898 <apkmanu1@gmail.com>
2024-10-15 20:41:54 +0200

    dd

--- servidor/proyecto/index.php
+++ servidor/proyecto/index.php
@@ -7,10 +7,45 @@
 
     use Config\Database;
     use Models\Employee;
-
-    $employees = Employee::all();  // SELECT
+    
+  
+    $employees = Employee::all();
+    
     echo '<pre>';
     print_r($employees);  // en comptes d'un foreach
     echo '</pre>';
-
+    
+    //crear taula per mostrar les dades
+    echo '<table border="1">';
+    echo '<tr>';
+    echo '<th>Employee ID</th>';
+    echo '<th>First Name</th>';
+    echo '<th>Last Name</th>';
+    echo '<th>Email</th>';
+    echo '<th>Phone Number</th>';
+    echo '<th>Hire Date</th>';
+    echo '<th>Job ID</th>';
+    echo '<th>Salary</th>';
+    echo '<th>Commission PCT</th>';
+    echo '<th>Manager ID</th>';
+    echo '<th>Department ID</th>';
+    echo '</tr>';
+    foreach ($employees as $employee) {
+        //td with styles widht 100px
+        echo '<tr>';
+        echo '<td style="height:100px;">' . $employee['employee_id'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['first_name'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['last_name'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['email'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['phone_number'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['hire_date'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['job_id'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['salary'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['commission_pct'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['manager_id'] . '</td>';
+        echo '<td style="height:100px;">' . $employee['department_id'] . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+   
 ?>
---
+++ servidor/proyecto/models/Customer.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace models;
+
+    class Cuestomer extends Model {
+        // Definir la taula associada a la classe
+        protected static $table = 'customers';
+
+    }
+
+?>
