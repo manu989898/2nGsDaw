a21c271661ad1155df0c2b8382a0cfb91a332664
manu989898 <apkmanu1@gmail.com>
2024-10-15 18:06:02 +0200

    react app

---
+++ my-app/src/data/series-static.js
@@ -0,0 +1,83 @@
+export const seriesList = [
+    {
+        "id":1,
+        "title":"Timeless",
+        "img":"similar-timeless.jpg",
+        "year":2018,
+        "pegi":16,
+        "matching":86,
+        "seasons":2,
+        "desc":"Una historiadora, un soldado y un programador viajan en el tiempo para perseguir a criminales que amenazan con perturbar el espacio-tiempo."
+    },
+    {
+        "id":2,
+        "title":"Jupiter's Legacy",
+        "img":"similars-2.jpg",
+        "year":2021,
+        "pegi":16,
+        "matching":50,
+        "episodes":8,
+        "desc":"Ellos son la primera generación de superhéroes. Ahora que deben pasar el testigo a sus hijos, las tensiones aumentan... y ya no valen las viejas normas."
+    },
+    {
+        "id":3,
+        "title":"The witcher",
+        "img":"similar-the-witcher.jpg",
+        "year":2021,
+        "pegi":16,
+        "matching":50,
+        "seasons":2,
+        "desc":"Geralt de Rivia, un cazador de monstruos mutante, viaja en pos de su destino por un mundo turbulento en el que, a menudo, los humanos son peores que las bestias."
+    },
+    {
+        "id":4,
+        "Title":"Sombra hueso",
+        "img":"similar-sombra-hueso.jpg",
+        "year":2021,
+        "pegi":12,
+        "matching":50,
+        "seasons":1,
+        "desc":"Fuerzas siniestras conspiran contra la cartógrafa huérfana Alina Starkov cuando desata un poder mágico capaz de cambiar el destino de su mundo, asolado por la guerra."
+    },
+    {
+        "id":5,
+        "Title":"Gotham",
+        "img":"similar-gotham.jpg",
+        "year":2019,
+        "pegi":16,
+        "matching":89,
+        "seasons":5,
+        "desc":"Mucho antes de ser inspector, el policía principiante James Gordon se enfrenta al crimen y corrupción de Gotham City para vengar la muerte de los padres de Bruce Wayne."
+    },
+    {
+        "id":6,
+        "Title":"Ollie",
+        "type":"miniserie",
+        "img":"similar-ollie.jpg",
+        "year":2022,
+        "pegi":7,
+        "matching":50,
+        "desc":"Un conejo hecho de retales, con grandes orejas y recuerdos borrosos, emprende una búsqueda épica de su mejor amigo: el niño al que quiere con todo su corazón."
+    },
+    {
+        "id":7,
+        "Title":"Los 100",
+        "img":"similars-los100.jpg",
+        "year":2020,
+        "pegi":16,
+        "matching":50,
+        "seasons":7,
+        "desc":"Un siglo después de que la Tierra resultase devastada por un apocalipsis nuclear, 100 habitantes de una estación espacial vuelven al planeta a comprobar si es habitable."
+    },
+    {
+        "id":8,
+        "Title":"Lost in space",
+        "img":"similars-lost-in-space.jpg",
+        "year":2021,
+        "pegi":7,
+        "matching":50,
+        "seasons":3,
+        "desc":"Tras un aterrizaje forzoso en un planeta desconocido, la familia Robinson lucha desesperadamente por sobrevivir y escapar, pero está rodeada de peligros ocultos."
+    },
+    
+];
